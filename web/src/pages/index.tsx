6182a7ff3a9a3577cd42e9ed626be768ecd863aa
Elcio Yoshiaki Sato <elciosato@gmail.com>
2022-11-05 23:45:49 +0100

    Adjust typo in url

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -93,7 +93,7 @@ export default function Home(props: HomeProps) {
 export const getServerSideProps = async () => {
   const [poolCountResponse, guessCountResponse, userCountResponse] =
     await Promise.all([
-      api.get("/pools/count"),
+      api.get("/polls/count"),
       api.get("/guesses/count"),
       api.get("/users/count"),
     ]);
